62d7b7c5a715c0fb7b3989583c380c1dc34fe094
Oğuz Kayra Büyükyıldırım <74321096+OguzKaira@users.noreply.github.com>
2021-07-21 15:37:39 +0300

    Create Write_Demo.py

---
+++ Write_Demo.py
@@ -0,0 +1,15 @@
+# Library
+import streamlit as st
+
+# Creating Title
+st.title("First App")
+
+# Header
+st.header("Why Streamlit?")
+st.text("""Being able to build and publish data apps with less time consuming than other Frameworks is
+a nice advantage, so it's useful to use.""")
+ 
+# Subheader
+st.subheader("Why You Need to Deploy?")
+st.text("""Presenting your projects to users (clients) can provide useful feedback, 
+and you can create a good portfolio by publishing these applications.""")
